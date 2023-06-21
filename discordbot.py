b7031edc8ad163b678ff465fa16d930285d685da
dike-okayama <lawliet.d.k.m@gmail.com>
2023-06-21 15:36:47 +0900

    Initial Commit

---
+++ discordbot.py
@@ -0,0 +1,22 @@
+import discord
+
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(
+    'MTA2NTYzOTA2OTQzMTkwMjI4OQ.GCA7yr.H-hTou2Yz1sTF_E93U9La5cHCwTGrPbdr881xQ')
