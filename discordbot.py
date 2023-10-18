e403ebee9dcb8d6f63466877a9787b81405aba11
Iida <2002kousei@gmail.com>
2023-10-19 02:23:51 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -226,12 +226,12 @@ async def loop():
                         value="https://pptx-maker.uoh-dakken.com/#/")
         await channel.send(embed=embed)
         
-    if week == 4 and hr == 2 and min == 23:  # 解析コンペ出欠embed
+    if week == 4 and hr == 2 and min == 25:  # 解析コンペ出欠embed
         channel = client.get_channel(TEST_CHANNEL_ID)
         if not isinstance(channel, discord.TextChannel):
             return
         embed = discord.Embed(
-            title="**{month}**月**{day}**日(**{week_list[week-1]}**) 解析コンペの出欠確認**",
+            title=f"**{month}**月**{day}**日(**{week_list[week-1]}**) 解析コンペの出欠確認**",
             description=f"{da_mention} 本日,解析コンペの会議に \n 出席 ⇒ {CIRCLE_ICON} , 欠席 ⇒ {CROSS_ICON}",
             color=discord.Colour.from_rgb(97, 216, 70))
         embed.set_thumbnail(
